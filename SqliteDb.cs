da0cf4a6a9f75cfa3270ff72dff6bd923747c74b
furkandmrz <furkanded00@gmail.com>
2025-12-29 21:02:38 +0300

    Add files via upload

---
+++ SqliteDb.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace MarketStokTakip.Database
+{
+    public class SQLiteDbConnection                          // Sqlite bağlantısı için kullanılan sınıf.
+    {
+        private static string yol = "Data Source=MarketStok.db";
+
+        public static SqliteConnection BaglantiGetir()
+        {
+            var con = new SqliteConnection(yol);
+            con.Open();
+            return con;
+        }
+    }
+}
