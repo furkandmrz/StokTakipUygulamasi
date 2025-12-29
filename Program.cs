caaf586226c8bd7e8bfc6ecdc2bd3a78ac099828
furkandmrz <furkanded00@gmail.com>
2025-12-29 21:14:39 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,5 @@
-namespace StokTakipUygulaması
-{
-    internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
-        }
-    }
-}
+using StokTakipUygulaması;
+using System.Windows.Forms;
+
+ApplicationConfiguration.Initialize();
+Application.Run(new Form1());
